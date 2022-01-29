4cef3f29d57694b6fe985fa852f0a945952f7757
Pomp3 <70841007+Pomp3@users.noreply.github.com>
2022-01-29 20:34:55 +0000

    Version 2.1.2

--- learner.py
+++ learner.py
@@ -138,3 +138,5 @@ def get_args():
                 ['test_file', 'model_input', 'prediction_file'],
                 []),
             (['fitpredict', 'fp'],
+                ['training_file', 'model', 'test_file', 'prediction_file'],
+                ['model_options', 'model_output']),
